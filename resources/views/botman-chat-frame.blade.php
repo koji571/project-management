31677d9d5ecc653499265376a501f4bbe3d30e1b
leotan <leotan.haiken@gmail.com>
2024-05-02 13:09:37 +0800

    localised assests

--- resources/views/botman-chat-frame.blade.php
+++ resources/views/botman-chat-frame.blade.php
@@ -4,9 +4,9 @@
 <head>
     <title>BotMan Widget</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/chat.css') }}">
 </head>
 <body>
-<script id="botmanWidget" src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/chat.js'></script>
+<script id="botmanWidget" src="{{ asset('js/chat.js') }}"></script>
 </body>
 </html>
